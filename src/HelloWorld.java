a80f785425b91ca20a96eafa33fe58263b1de84e
yjkim <chitose_@naver.com>
2021-02-24 15:17:31 +0900

    세번쨰 라인 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hello's end Dancehall");
+		System.out.println("Earth's end Dancehall");
 
 	}
 
